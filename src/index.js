7eabc1d38653516dc200e9358fa11167c814e93d
Lena Pavlenko <pawlencko.alena2010@yandex.ru>
2023-10-10 11:57:52 +0300

    добавление конфигурационных файлов и файлов для теста

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './index.html';
+import './scss/index.scss';
+
+
+const img = document.querySelector('img')
+
+console.log(img);
---
+++ webpack.config.js
@@ -0,0 +1,128 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const mode = process.env.NODE_ENV || 'development';
+const devMode = mode === 'development';
+const target = devMode ? 'web' : 'browserslist';
+const devtool = devMode ? 'source-map' : undefined;
+
+module.exports = {
+    mode,
+    target,
+    devtool,
+    devServer: {
+        port: 3000,
+        open: true,
+        hot: true,
+    },
+    entry: ['@babel/polyfill', path.resolve(__dirname, 'src', 'index.js')],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+        filename: devMode ? 'js/[name].[contenthash].js' : 'js/[name].js',
+        assetModuleFilename: 'assets/[name][ext]'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src', 'index.html'),
+            minify: false,
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].css',
+            ignoreOrder: false
+        }),
+    ],
+    module: {
+        rules: [
+            // html
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+                options: {
+                    minimize: false
+                },
+            },
+            // styles
+            {
+                test: /\.(с|sa|sc)ss$/i,
+                use: [
+                    devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    {
+                      loader: 'postcss-loader',
+                      options: {
+                        postcssOptions: {
+                          plugins: [require('postcss-preset-env')],
+                        },
+                      },
+                    },
+                    'group-css-media-queries-loader',
+                    {
+                      loader: 'resolve-url-loader',
+                    },
+                    {
+                      loader: 'sass-loader',
+                      options: {
+                        sourceMap: true,
+                      },
+                    },
+                  ],
+            },
+            // fonts
+            {
+                test: /\.(woff|woff2|ttf|otf|eot)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'fonts/[name][ext]'
+                }
+            },
+            // images
+            {
+                test: /\.(jpe?g|png|webp|gif|svg)$/i,
+                use: devMode
+                    ? []
+                    : [
+                        {
+                            loader: 'image-webpack-loader',
+                            options: {
+                                mozjpeg: {
+                                    progressive: true,
+                                },
+                                optipng: {
+                                    enabled: false,
+                                },
+                                pngquant: {
+                                    quality: [0.65, 0.9],
+                                    speed: 4,
+                                },
+                                gifsicle: {
+                                    interlaced: false,
+                                },
+                                webp: {
+                                    quality: 75,
+                                },
+                            },
+                        },
+                    ],
+                type: 'asset/resource',
+            },
+            // babel js
+            {
+                test: /\.(?:js|mjs|cjs)$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: [
+                      ['@babel/preset-env', { targets: "defaults" }]
+                    ]
+                  }
+                }
+            },
+
+            
+        ]
+    },
+}
