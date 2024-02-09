a11fe1ea00c869637165a1eed0dd50fe8e80e4ac
Lena Pavlenko <pawlencko.alena2010@yandex.ru>
2024-02-09 17:28:48 +0300

    верстка поиска, попапа корзины, фикс шапки

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import './scss/index.scss';
 
 import Swiper from 'swiper/bundle';
 
-if (document.querySelector('[data-swiper="banner"]')) {
+window.onload = function() {
+  const header = document.querySelector('.header')
+  const scrollHeader = header.querySelectorAll('.header__row')[1]
+  const searchBlock = document.querySelector('.search-block')
+
+  // Слайдер баннеров
+  if (document.querySelector('[data-swiper="banner"]')) {
     const swiperBanner = new Swiper('[data-swiper="banner"]', {
         loop: true,
         autoplay: true,
@@ -16,7 +22,52 @@ if (document.querySelector('[data-swiper="banner"]')) {
           prevEl: '[data-swiperNav="bannerNext"]',
         },
     });
-    console.log(swiperBanner.el);
+  }
+
+  // События при скролле
+  window.addEventListener('scroll', e => {
+    const headerPaddingTop = parseInt(window.getComputedStyle(header).paddingTop)
+    const topHeaderHeight = header.querySelectorAll('.header__row')[0].clientHeight
+    
+    let menuWidth = document.querySelector('.menu-products').clientWidth
+    
+    // Фикс шапки
+    if (window.scrollY > topHeaderHeight + headerPaddingTop) {
+      scrollHeader.classList.add('header-scroll')
+      document.body.style.paddingTop = `${topHeaderHeight + headerPaddingTop}px`
+    } else {
+      scrollHeader.classList.remove('header-scroll')
+      document.body.style.paddingTop = ''
+    }
+  
+    if (scrollHeader.classList.contains('header-scroll')) {
+      header.classList.add('isScroll')
+    } else {
+      header.classList.remove('isScroll')
+    }
+
+    // Открытие/закрытие поиска
+    if (searchBlock) {
+      const searchTrigger = searchBlock.querySelector('.search-block__btn')
+      const searchControl = searchBlock.querySelector('.search-block__control')
+    
+      searchTrigger.addEventListener('click', e => {
+        searchControl.classList.toggle('open')
+        searchControl.style.width = `${menuWidth}px`
+        if (searchControl.classList.contains('open')) {
+          menuWidth = 0
+        } else {
+          menuWidth = document.querySelector('.menu-products').clientWidth
+        }
+      })
+    }
+  })
+  
 }
+
+
+
+
+
 
 
