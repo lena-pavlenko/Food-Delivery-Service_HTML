7eba95dc587049da9a95949bc412536c880638e6
Lena Pavlenko <pawlencko.alena2010@yandex.ru>
2024-07-01 23:47:44 +0300

    адаптив

--- webpack.config.js
+++ webpack.config.js
@@ -9,124 +9,122 @@ const target = devMode ? 'web' : 'browserslist';
 const devtool = devMode ? 'source-map' : undefined;
 
 module.exports = {
-    mode,
-    target,
-    devtool,
-    devServer: {
-        port: 3000,
-        open: true,
-        hot: true,
-    },
-    entry: ['@babel/polyfill', path.resolve(__dirname, 'src', 'index.js')],
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-        filename: devMode ? 'js/[name].[contenthash].js' : 'js/[name].js',
-        assetModuleFilename: 'assets/[name][ext]'
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'src', 'index.html'),
-            minify: false,
-        }),
-        new HtmlWebpackPlugin({  // Also generate a test.html
-            filename: 'product.html',
-            template: path.resolve(__dirname, 'src', 'product.html')
-          }),
-        new MiniCssExtractPlugin({
-            filename: 'css/[name].css',
-            ignoreOrder: false
-        }),
-    ],
-    module: {
-        rules: [
-            // html
+  mode,
+  target,
+  devtool,
+  devServer: {
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+  entry: ['@babel/polyfill', path.resolve(__dirname, 'src', 'index.js')],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    filename: devMode ? 'js/[name].[contenthash].js' : 'js/[name].js',
+    assetModuleFilename: 'assets/[name][ext]'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+      minify: false,
+    }),
+    new HtmlWebpackPlugin({  // Also generate a test.html
+      filename: 'product.html',
+      template: path.resolve(__dirname, 'src', 'product.html')
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css',
+      ignoreOrder: false
+    }),
+  ],
+  module: {
+    rules: [
+      // html
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+        options: {
+            minimize: false
+        },
+      },
+      // styles
+      {
+        test: /\.(с|sa|sc)ss$/i,
+        use: [
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [require('postcss-preset-env')],
+              },
+            },
+          },
+          // 'group-css-media-queries-loader',
+          {
+            loader: 'resolve-url-loader',
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      // fonts
+      {
+        test: /\.(woff|woff2|ttf|otf|eot)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name][ext]'
+        }
+      },
+      // images
+      {
+        test: /\.(jpe?g|png|webp|gif|svg)$/i,
+        use: devMode
+        ? []
+        : [
             {
-                test: /\.html$/i,
-                loader: 'html-loader',
-                options: {
-                    minimize: false
+              loader: 'image-webpack-loader',
+              options: {
+                mozjpeg: {
+                  progressive: true,
                 },
+                optipng: {
+                  enabled: false,
+                },
+                pngquant: {
+                  quality: [0.65, 0.9],
+                  speed: 4,
+                },
+                gifsicle: {
+                  interlaced: false,
+                },
+                webp: {
+                  quality: 75,
+                },
+              },
             },
-            // styles
-            {
-                test: /\.(с|sa|sc)ss$/i,
-                use: [
-                    devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    {
-                      loader: 'postcss-loader',
-                      options: {
-                        postcssOptions: {
-                          plugins: [require('postcss-preset-env')],
-                        },
-                      },
-                    },
-                    'group-css-media-queries-loader',
-                    {
-                      loader: 'resolve-url-loader',
-                    },
-                    {
-                      loader: 'sass-loader',
-                      options: {
-                        sourceMap: true,
-                      },
-                    },
-                ],
-            },
-            // fonts
-            {
-                test: /\.(woff|woff2|ttf|otf|eot)$/,
-                type: 'asset/resource',
-                generator: {
-                    filename: 'fonts/[name][ext]'
-                }
-            },
-            // images
-            {
-                test: /\.(jpe?g|png|webp|gif|svg)$/i,
-                use: devMode
-                    ? []
-                    : [
-                        {
-                            loader: 'image-webpack-loader',
-                            options: {
-                                mozjpeg: {
-                                    progressive: true,
-                                },
-                                optipng: {
-                                    enabled: false,
-                                },
-                                pngquant: {
-                                    quality: [0.65, 0.9],
-                                    speed: 4,
-                                },
-                                gifsicle: {
-                                    interlaced: false,
-                                },
-                                webp: {
-                                    quality: 75,
-                                },
-                            },
-                        },
-                    ],
-                type: 'asset/resource',
-            },
-            // babel js
-            {
-                test: /\.(?:js|mjs|cjs)$/,
-                exclude: /node_modules/,
-                use: {
-                  loader: 'babel-loader',
-                  options: {
-                    presets: [
-                      ['@babel/preset-env', { targets: "defaults" }]
-                    ]
-                  }
-                }
-            },
-
-            
-        ]
-    },
+          ],
+          type: 'asset/resource',
+      },
+      // babel js
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        }
+      },
+    ]
+  },
 }
